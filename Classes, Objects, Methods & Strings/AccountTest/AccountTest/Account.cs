a698d11900ed1d75f7627a9b75403432ef385ca8
Sebastian Quintero Arias <bastian284c@gmail.com>
2026-01-02 23:05:32 -0500

    Account

---
+++ Classes, Objects, Methods & Strings/AccountTest/AccountTest/Account.cs
@@ -0,0 +1,18 @@
+﻿
+class Account
+{
+
+    private string name;
+    // instance variable
+    // method that sets the account name in the object
+
+    public void SetName(string accountName)
+    {
+        name = accountName; // store the account name
+    }
+    // method that retrieves the account name from the object
+    public string GetName()
+    {
+        return name; // returns name's value to this method's caller }
+    }
+}
